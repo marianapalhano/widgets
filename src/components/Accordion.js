c8c0457e874c2516d89cf2b1fcbdee4e59c2bdd4
Mariana <marianaufc@gmail.com>
2021-08-18 17:15:06 -0300

    Finish Accordion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -8,13 +8,15 @@ const Accordion = ({ items }) => {
     }
 
     const renderedItems = items.map((item, index) => {
+        const active = activeIndex === index ? 'active' : ''; 
+
         return(
             <React.Fragment key={item.title}>
-                <div className="title active" onClick={() => onTitleClick(index)}>
+                <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
@@ -24,7 +26,6 @@ const Accordion = ({ items }) => {
     return(
         <div className="ui styled accordion">
             {renderedItems}
-            {activeIndex}
         </div>
     )
 }
