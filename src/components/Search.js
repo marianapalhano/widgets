15064e71d232247696609f74402a5a733003bc1d
Mariana <marianaufc@gmail.com>
2021-08-30 09:15:33 -0300

    Added useEffect hoook

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function Search() {
     const [term, setTerm] = useState('');
 
+    useEffect(() => {
+        console.log('asdf')
+    });
+
     function searchWiki(input) {
-        console.log(input);
         setTerm(input);
     }
 
