dae7ccd4543bb1e3aff5ae4d4dcdf4d095d9deb3
Mariana <marianaufc@gmail.com>
2021-09-15 17:02:58 -0300

    Dropdown changing with selected item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Dropdown from './components/Dropdown';
 import Search from './components/Search';
@@ -34,13 +34,14 @@ const options = [
 ]
 
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
     return(
         <div>
             <Accordion items={items} />
             <hr />
             <Search />
             <hr />
-            <Dropdown options={options} />
+            <Dropdown options={options} selected={selected} onSelectedChange={setSelected} />
         </div>
     )
 };
